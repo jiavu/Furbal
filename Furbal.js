83114203cd79db002c8d301d4edc0927178ea9e8
jiavu <39624614+jiavu@users.noreply.github.com>
2018-09-25 02:16:42 +0200

    1st draft of natural decreases and increases
    Power levels, decrease levels of main basic need parameters

--- Furbal.js
+++ Furbal.js
@@ -10,6 +10,7 @@ const colorMap = ["ff0000", "ff0000", "ff0000", "ff0000", "ff0000", "ff0000", "f
 // Control monitor:
 const timeElapsed = document.getElementById("timeElapsed");
 const loopSpeed = document.getElementById("loopSpeed");
+const gSpeed = document.getElementById("gSpeed");
 const interv = document.getElementById("interv");
 const count = document.getElementById("count");
 const pauseButton = document.getElementById("pause");
@@ -18,7 +19,7 @@ const pauseButton = document.getElementById("pause");
 const health = document.getElementById("health");
 const satiation = document.getElementById("satiation");
 const fun = document.getElementById("fun");
-const care = document.getElementById("care");
+const secureness = document.getElementById("secureness");
 
 //////////////////////////////////////////////////
 
@@ -26,34 +27,46 @@ const care = document.getElementById("care");
 let lastRender = 0;
 let progress = 0;
 let v_timeElapsed = 0;
-let counter = 0;
-let interval = 5;
+const gameSpeed = 20;
+let counter = 0;            // counter and interval are not so
+let interval = 1;           // important anymore. Maybe delete
 let pause = false;
 
-// Balancing decrease:
+// Balancing powers of decrease/increase:
 let healthUpdate;
-const gameSpeed = 1; // deprecated. ^^ Delete later or find a solution
-const naturalDecreaseOfSatiation = 0.4;
-const satiationPower = 0.01;
-const naturalDecreaseOfFun = 0.2;
-const funPower = 0.01;
-const naturalDecreaseofLove = 0.5;
+const naturalDecreaseOfSatiation = 0.01;
+const satiationPower = 0.001;               // character traits will influence the Power variables
+const naturalDecreaseOfFun = 0.005;
+const funPower = 0.0004;                    //
+const naturalDecreaseOfSecureness = 0.015;
+const securenessPower = 0.0003;             //
+
 
 // Furball:
 const myFurball = {
     isDead : false,
-    health : 100,
+    health : 1,
     satiation : 100,
     fun : 100,
-    love : 50
+    secureness : 100
 }
 
 //////////////////////////////////////////////////
+/*
+8009: -11.340799707890277
+8016: -12.644385768130062
+8009: -11.047424574986625
+*/
 //////////////////////////////////////////////////
 
 // Functions:
 
-const gameOver = () => window.alert("Game Over!");
+const gameOver = () => {
+    window.alert("Game Over!");
+    console.log("Health: ", myFurball.health-100);
+    console.log("/2: ", (myFurball.health-100)/2);
+    console.log("/4: ", (myFurball.health-100)/4);
+};
 
 
 const switchPause = () => {
@@ -70,37 +83,36 @@ function update(progress) {
     if (counter == interval) {
         counter = 0;
 
-        myFurball.satiation -= naturalDecreaseOfSatiation;
-        if (myFurball.satiation <= 0) myFurball.satiation = 0;
-        myFurball.fun -= naturalDecreaseOfFun;
-        if (myFurball.fun <= 0) myFurball.fun = 0;
+        myFurball.satiation -= naturalDecreaseOfSatiation * gameSpeed;  // + character trait
+        myFurball.fun -= naturalDecreaseOfFun * gameSpeed;              // + character trait
+        myFurball.secureness -= naturalDecreaseOfSecureness * gameSpeed;// + character trait
 
         // My own formula, makes not much sense. Or does it?
         //healthUpdate = -(satiationPower/myFurball.satiation)// + funPower/myFurball.fun);
         
-        // Newtons Law of Cooling:
-        healthUpdate = -satiationPower*(myFurball.health - myFurball.satiation);
+        // Newtons Law of Cooling: u'(t) = -k(u(t)-a)
+        healthUpdate = -satiationPower*(myFurball.health-myFurball.satiation)-funPower*(myFurball.health-myFurball.fun)-securenessPower*(myFurball.health-myFurball.secureness);
                 
-        
         myFurball.health += healthUpdate;
 
     }
     if (myFurball.health >= 100) myFurball.health = 100;
-    if (myFurball.health <= 0.01) myFurball.isDead = true;
-    //if (v_timeElapsed >= 10000) { myFurball.isDead = true } // Control values after a certain time. Delete later.
+    if (myFurball.health <= 0) myFurball.isDead = true;
+    //if (v_timeElapsed >= 8000) { myFurball.isDead = true } // Control values after a certain time. Delete later.
  };
 
 
 function draw() {
 
     timeElapsed.innerHTML = "Time Elapsed: " + Math.round(v_timeElapsed) + "ms";
-    loopSpeed.innerHTML = "Game Loop Speed: " + Math.round(progress) + "ms/loop";
+    loopSpeed.innerHTML = "Loop Speed: " + Math.round(progress) + "ms/loop";
+    gSpeed.innerHTML = "Game Speed: " + gameSpeed;
     interv.innerHTML = "Interval: " + interval;
 
     health.children[0].style.width = myFurball.health + "%";
     health.children[0].style.backgroundColor = "#" + colorMap[Math.round(myFurball.health-1)]; // Achtung, Index -1 gibt es nicht.
 
-    health.children[0].innerHTML = myFurball.health // for controlling. Delete later
+    //health.children[0].innerHTML = myFurball.health // for controlling. Delete later
 
     health.style.borderColor = "#" + colorMap[Math.round(myFurball.health-1)];
     satiation.children[0].style.width = myFurball.satiation + "%";
@@ -109,9 +121,9 @@ function draw() {
     fun.children[0].style.width = myFurball.fun + "%";
     fun.children[0].style.backgroundColor = "#" + colorMap[Math.round(myFurball.fun-1)];
     fun.style.borderColor = "#" + colorMap[Math.round(myFurball.fun-1)];
-    care.children[0].style.width = myFurball.care + "%";
-    care.children[0].style.backgroundColor = "#" + colorMap[Math.round(myFurball.care-1)];
-    care.style.borderColor = "#" + colorMap[Math.round(myFurball.care-1)];
+    secureness.children[0].style.width = myFurball.secureness + "%";
+    secureness.children[0].style.backgroundColor = "#" + colorMap[Math.round(myFurball.secureness-1)];
+    secureness.style.borderColor = "#" + colorMap[Math.round(myFurball.secureness-1)];
 
 }
 
