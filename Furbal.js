a116ee401410cb4def65927acd1642c046ee1f74
jiavu <39624614+jiavu@users.noreply.github.com>
2018-09-26 02:44:12 +0200

    Mutual influencing of parameters

--- Furbal.js
+++ Furbal.js
@@ -28,24 +28,44 @@ let lastRender = 0;
 let progress = 0;
 let v_timeElapsed = 0;
 const gameSpeed = 20;
+/*
+Health decrease on different gameSpeed values:
+
+-2.4551341677638163  8012ms  gameSpeed 1
+-2.426407073306393  4007ms  gameSpeed 2
+-2.4214364966589557 2011ms   gameSpeed 4
+
+-7.531170623291359 16008ms gameSpeed 1
+-7.505613495337315 8015ms gameSpeed 2
+-7.502141006606919 4013ms gameSpeed 4
+
+-20.016419080736142 32006ms gameSpeed 1
+-19.879081228891607 16002ms gameSpeed 2
+-19.74145722168376 8005ms gameSpeed 4
+*/
 let counter = 0;            // counter and interval are not so
 let interval = 1;           // important anymore. Maybe delete
 let pause = false;
 
 // Balancing powers of decrease/increase:
 let healthUpdate;
-const naturalDecreaseOfSatiation = 0.01;
-const satiationPower = 0.001;               // character traits will influence the Power variables
-const naturalDecreaseOfFun = 0.005;
+const naturalDecreaseOfSatiation = 0.015;
+const satiationPower = 0.0015;               // character traits will influence the Power variables
+const naturalDecreaseOfFun = 0.0078;
 const funPower = 0.0004;                    //
-const naturalDecreaseOfSecureness = 0.015;
+const naturalDecreaseOfSecureness = 0.013;
 const securenessPower = 0.0003;             //
 
+//const funToSat = 0.00007;   // the more fun, the higher satiation decrease. Use: funToSat+fun | DELETE, CHANGED!!! playButton => decreases satiation
+const healthToFun = 0.01;      // the less health, the higher fun decrease. Use: -(healthToFun/100)*health + healthToFun
+const healthToSec = 0.0115;
+
+//////////////////////////////////////////////////
 
 // Furball:
 const myFurball = {
     isDead : false,
-    health : 1,
+    health : 100,
     satiation : 100,
     fun : 100,
     secureness : 100
@@ -53,9 +73,6 @@ const myFurball = {
 
 //////////////////////////////////////////////////
 /*
-8009: -11.340799707890277
-8016: -12.644385768130062
-8009: -11.047424574986625
 */
 //////////////////////////////////////////////////
 
@@ -83,17 +100,17 @@ function update(progress) {
     if (counter == interval) {
         counter = 0;
 
-        myFurball.satiation -= naturalDecreaseOfSatiation * gameSpeed;  // + character trait
-        myFurball.fun -= naturalDecreaseOfFun * gameSpeed;              // + character trait
-        myFurball.secureness -= naturalDecreaseOfSecureness * gameSpeed;// + character trait
+        myFurball.satiation -= (naturalDecreaseOfSatiation /* + myFurball.fun*funToSat */) * gameSpeed;               // + character trait
+        myFurball.fun -= (naturalDecreaseOfFun + -healthToFun/100*myFurball.health + healthToFun) * gameSpeed;              // + character trait
+        myFurball.secureness -= (naturalDecreaseOfSecureness + -healthToSec/100*myFurball.health + healthToSec) * gameSpeed;// + character trait
 
         // My own formula, makes not much sense. Or does it?
         //healthUpdate = -(satiationPower/myFurball.satiation)// + funPower/myFurball.fun);
         
         // Newtons Law of Cooling: u'(t) = -k(u(t)-a)
         healthUpdate = -satiationPower*(myFurball.health-myFurball.satiation)-funPower*(myFurball.health-myFurball.fun)-securenessPower*(myFurball.health-myFurball.secureness);
-                
-        myFurball.health += healthUpdate;
+        //myFurball.health -= 0.01 * gameSpeed;// Test only, DELETE!!!!
+        myFurball.health += healthUpdate * gameSpeed;
 
     }
     if (myFurball.health >= 100) myFurball.health = 100;
@@ -112,7 +129,9 @@ function draw() {
     health.children[0].style.width = myFurball.health + "%";
     health.children[0].style.backgroundColor = "#" + colorMap[Math.round(myFurball.health-1)]; // Achtung, Index -1 gibt es nicht.
 
-    //health.children[0].innerHTML = myFurball.health // for controlling. Delete later
+    // for controlling. Delete later:
+    health.children[0].innerHTML = myFurball.health;
+    //satiation.children[0].innerHTML = myFurball.satiation;
 
     health.style.borderColor = "#" + colorMap[Math.round(myFurball.health-1)];
     satiation.children[0].style.width = myFurball.satiation + "%";
