f14541f218f9bc7704fd067a075182bad52c5feb
jiavu <cs@CS-Germany-iMac.fritz.box>
2018-12-19 14:52:16 +0100

    Fixed (made-by-)credits layout

--- additional_files/furbal_infoText.js
+++ additional_files/furbal_infoText.js
@@ -92,6 +92,7 @@ const infoText = {
                 Lemon and clover leaf made by <a href="http://www.freepik.com" target="_blank" title="Freepik">Freepik</a><br> \
                 Santa hat made by <a href="https://www.flaticon.com/authors/vectors-market" target="_blank" title="Vectors Market">Vectors Market</a><br> \
                 from <a href="https://www.flaticon.com/" target="_blank" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a> \
+                <br> \
             </div> \
         <p><button type="button" class="smaller-button" id="back">Back</button></p> \
         </div>',
